d18de1f20075295476f6708e2ee041c977f8cc78
brnandhu001 <85947143+brnandhu001@users.noreply.github.com>
2021-07-31 20:58:43 +0530

    Add files via upload

---
+++ wallet.31.7.21/client/src/connection.js
@@ -0,0 +1,30 @@
+import {useWallet,UseWalletProvider} from 'use-wallet'
+import React from 'react'
+
+
+function Connection(){
+    const wallet=useWallet()
+    const connectwallet =async(e)=>{
+        e.preventDefault()
+        await wallet.connect()
+    }
+    return(
+        <div>
+            <button onClick={connectwallet}>connectwallet</button>
+        </div>
+    )
+}
+
+// Wrap everything in <UseWalletProvider />
+export default () => (
+    <UseWalletProvider
+      chainId={1337}
+      connectors={{
+        // This is how connectors get configured
+        provided: { provided: window.cleanEthereum },
+      }}
+    >
+      {/* <App /> */}
+      <Connection/>
+    </UseWalletProvider>
+  )
